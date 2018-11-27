1dcabe2cbdf173e7fab3edca6be506c82711abad
Nicholas Sheward <nick.sheward@bbc.co.uk>
2018-11-27 17:35:06 +0000

    added promises

---
+++ promises/index.js
@@ -0,0 +1,22 @@
+/*
+const example1 = {
+	exampleMethod() => {
+		return new Promise((resolve, reject) => {
+			rmsClient.getProgrammes().then((res)=>resolve(res)).catch((err)=>reject(err));
+		})
+	}
+}
+
+const example2 = {
+	exampleMethod() => {
+		return new Promise(async (resolve, reject) => {
+      try {
+        const prog = await rmsClient.getProgrammes();
+        resolve(prog);
+      } catch(e) {
+        reject(e);
+      }
+		})
+	}
+}
+*/
